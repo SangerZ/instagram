710af9d64d6d3079e60f86e201c02a38aca3db5b
SangerZ <ratversion2@msn.com>
2017-11-24 17:18:04 +0700

    asf
    asdf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,67 @@
-import { Component } from '@angular/core';
-import { Camera } from '@ionic-native/camera';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { FirebaseListObservable, AngularFire } from 'angularfire2';
 
-import { Photo } from "../photo";
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  
-  constructor(private camera: Camera) { }
+export class HomePage implements OnInit {
 
-  photos: Photo[] = [new Photo("http://placehold.it/350x150", 5), new Photo("http://placehold.it/350x151", 6)];
+  constructor(private af: AngularFire, private camera: Camera, public alerCtrl: AlertController) { }
+
+  photos: FirebaseListObservable<any>;
+
+  ngOnInit() {
+    this.photos = this.af.database.list('/photos')
+  }
 
   takePhoto() {
-    this.camera.getPicture({
-      destinationType: this.camera.DestinationType.DATA_URL,
-      targetHeight: 500,
-      targetWidth: 500,
-      correctOrientation: true
-    }).then((imageData) => {
-      this.photos.push(new Photo("data:image/jpeg;base64," + imageData, 0));
-    }, (err) => {
-      console.log(err);
+    console.log("peanut");
+
+    let confirm = this.alerCtrl.create({
+      title: 'Open the camera?',
+      message: 'Do you agree to allow us to use your camera?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+
+            const option: CameraOptions = {
+              quality: 50,
+              destinationType: this.camera.DestinationType.DATA_URL,
+              targetHeight: 500,
+              targetWidth: 500,
+              mediaType: this.camera.MediaType.PICTURE,
+              correctOrientation: true
+            }
+            this.camera.getPicture(option).then((imageData) => {
+              this.photos.push({ src: "data:image/jpeg;base64," + imageData, likes: 0 });
+            }, (err) => {
+              console.log(err);
+            });
+          }
+        }
+      ]
     });
   }
 
-  deletePhoto(photo) {
-    this.photos.splice(this.photos.indexOf(photo), 1);
+  deletePhoto(photoKey: string) {
+    //this.photos.splice(this.photos.indexOf(photo), 1);
+    this.photos.remove(photoKey);
   }
 
-  likePhoto(photo) {
-    photo.likes++;
+  likePhoto(photoKey: string, likes: number) {
+    //photo.likes++;
+    this.photos.update(photoKey, { likes: likes + 1 });
   }
 }
